110ad702049c194fc738dfe68ee40a23d55de7e2
Sam Fareez <vsefareez@gmail.com>
2025-08-04 20:32:14 +0300

    Add provisioning scripts for vastai: default, flux_krea, wan2.2

---
+++ vastai/wan2.2.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+source /venv/main/bin/activate
+COMFYUI_DIR=${WORKSPACE}/ComfyUI
+
+APT_PACKAGES=()
+PIP_PACKAGES=()
+NODES=(
+    "https://github.com/ltdrdata/ComfyUI-Manager"
+    "https://github.com/cubiq/ComfyUI_essentials"
+)
+
+WORKFLOWS=(
+    "https://raw.githubusercontent.com/Comfy-Org/workflow_templates/refs/heads/main/templates/video_wan2_2_14B_t2v.json"
+)
+
+VAE_MODELS=(
+    "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors"
+)
+
+TEXT_ENCODERS=(
+    "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensornitis"
+)
+
+DIFFUSION_MODELS=(
+    "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_t2v_high_noise_14B_fp8_scaled.safetensors"
+)
+
+function provisioning_start() {
+    provisioning_print_header
+    provisioning_get_apt_packages
+    provisioning_get_nodes
+    provisioning_get_pip_packages
+
+    workflows_dir="${COMFYUI_DIR}/user/default/workflows"
+    mkdir -p "${workflows_dir}"
+    provisioning_get_files "${workflows_dir}" "${WORKFLOWS[@]}"
+    provisioning_get_files "${COMFYUI_DIR}/models/vae" "${VAE_MODELS[@]}"
+    provisioning_get_files "${COMFYUI_DIR}/models/text_encoders" "${TEXT_ENCODERS[@]}"
+    provisioning_get_files "${COMFYUI_DIR}/models/diffusion_models" "${DIFFUSION_MODELS[@]}"
+
+    provisioning_print_end
+}
+
+function provisioning_get_apt_packages() {
+    if [[ -n $APT_PACKAGES ]]; then
+        sudo $APT_INSTALL ${APT_PACKAGES[@]}
+    fi
+}
+
+function provisioning_get_pip_packages() {
+    if [[ -n $PIP_PACKAGES ]]; then
+        pip install --no-cache-dir ${PIP_PACKAGES[@]}
+    fi
+}
+
+function provisioning_get_nodes() {
+    for repo in "${NODES[@]}"; do
+        dir="${repo##*/}"
+        path="${COMFYUI_DIR}/custom_nodes/${dir}"
+        requirements="${path}/requirements.txt"
+        if [[ -d $path ]]; then
+            if [[ ${AUTO_UPDATE,,} != "false" ]]; then
+                echo "Updating node: ${repo}"
+                ( cd "$path" && git pull )
+                [[ -e $requirements ]] && pip install --no-cache-dir -r "$requirements"
+            fi
+        else
+            echo "Downloading node: ${repo}"
+            git clone "${repo}" "${path}" --recursive
+            [[ -e $requirements ]] && pip install --no-cache-dir -r "$requirements"
+        fi
+    done
+}
+
+function provisioning_get_files() {
+    [[ -z $2 ]] && return 1
+    dir="$1"
+    mkdir -p "$dir"
+    shift
+    arr=("$@")
+    echo "Downloading ${#arr[@]} file(s) to $dir..."
+    for url in "${arr[@]}"; do
+        echo "Downloading: $url"
+        provisioning_download "$url" "$dir"
+        echo
+    done
+}
+
+function provisioning_print_header() {
+    echo -e "\\n##############################################"
+    echo -e "#          Provisioning container            #"
+    echo -e "#         This will take some time           #"
+    echo -e "# Your container will be ready on completion #"
+    echo -e "##############################################\\n"
+}
+
+function provisioning_print_end() {
+    echo -e "\\nProvisioning complete: Application will start now\\n"
+}
+
+function provisioning_download() {
+    if [[ -n $HF_TOKEN && $1 =~ ^https://([a-zA-Z0-9_-]+\\.)?huggingface\\.co(/|$|\\?) ]]; then
+        auth_token="$HF_TOKEN"
+    fi
+    if [[ -n $auth_token ]]; then
+        wget --header="Authorization: Bearer $auth_token" -qnc --content-disposition --show-progress -e dotbytes=4M -P "$2" "$1"
+    else
+        wget -qnc --content-disposition --show-progress -e dotbytes=4M -P "$2" "$1"
+    fi
+}
+
+if [[ ! -f /.noprovisioning ]]; then
+    provisioning_start
+fi
